17dac8942c81eac4bb015c20b5e2f352c191468a
DmutroSheva <shevche3@gmail.com>
2025-04-21 13:31:10 +0300

    make build

---
+++ index.756fec69.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){var e=document.querySelector("table");["thead","tbody","tfoot"].forEach(function(t){e.querySelectorAll("".concat(t," tr")).forEach(function(e){var t=e.children,o=t[1].cloneNode(!0);e.insertBefore(o,t[t.length-1])})})});
+//# sourceMappingURL=index.756fec69.js.map
