32e01992b47a53797bc272173125fcc511ca5efc
DmutroSheva <shevche3@gmail.com>
2025-04-21 13:35:00 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,11 @@
 'use strict';
 
-document.addEventListener('DOMContentLoaded', () => {
-  const table = document.querySelector('table');
+const table = document.querySelector('table');
+const rows = table.rows;
 
-  ['thead', 'tbody', 'tfoot'].forEach((section) => {
-    const rows = table.querySelectorAll(`${section} tr`);
+for (let i = 0; i < rows.length; i++) {
+  const cell = rows[i].cells;
+  const positions = cell[1].cloneNode(true);
 
-    rows.forEach((row) => {
-      const cells = row.children;
-
-      const clonedCell = cells[1].cloneNode(true);
-
-      row.insertBefore(clonedCell, cells[cells.length - 1]);
-    });
-  });
-});
+  rows[i].insertBefore(positions, cell[cell.length - 1]);
+}
